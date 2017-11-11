7adc827678926f966a06080b49d70313b9cb72e4
James Harton <james@rabidtech.co.nz>
2017-11-12 11:51:36 +1300

    Complete function calling example.

--- src/acceptance/functions.rs
+++ src/acceptance/functions.rs
@@ -46,5 +46,43 @@ fn add(machine: &mut Machine<Operand>, _args: &[usize]) {
 fn call(machine: &mut Machine<Operand>, args: &[usize]) {
     let label = machine.get_data(args[0]).to_s().unwrap();
     // let label = machine.builder.data[args[0]].to_s().unwrap();
-    machine.jump(label)
+    machine.jump(label);
+}
+
+fn ret(machine: &mut Machine<Operand>, _args: &[usize]) {
+    machine.ret();
+}
+
+fn instruction_table() -> InstructionTable<Operand> {
+    let mut it = InstructionTable::new();
+    it.insert(Instruction::new(0, "push", 1, push));
+    it.insert(Instruction::new(1, "add",  0, add));
+    it.insert(Instruction::new(2, "call", 1, call));
+    it.insert(Instruction::new(3, "ret",  0, ret));
+    it
+}
+
+fn op_i(i: i64) -> Operand {
+    Operand::I(i)
+}
+
+fn op_s(s: &str) -> Operand {
+    Operand::S(String::from(s))
+}
+
+#[test]
+fn example() {
+    let it = instruction_table();
+    let mut builder: Builder<Operand> = Builder::new(&it);
+    builder.push(0, vec![op_i(3)]);
+    builder.push(0, vec![op_i(4)]);
+    builder.push(2, vec![op_s("add_fun")]);
+    builder.label("add_fun");
+    builder.push(1, vec![]);
+    builder.push(3, vec![]);
+    let constants: MutableTable<Operand> = MutableTable::new();
+    let machine: Machine<Operand> = Machine::new(builder, &constants);
+    let mut machine = Machine::run(machine);
+    let result = machine.operand_pop().to_i().unwrap();
+    assert_eq!(result, 7);
 }
