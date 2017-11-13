1e5d0b52f18540607bfbd9272e0f81b711259c72
James Harton <james@automat.nz>
2017-11-14 09:07:33 +1300

    Bytecode loading works now.

--- src/code.rs
+++ src/code.rs
@@ -1,9 +1,10 @@
 use std::fmt;
-use std::io::Write;
-use rmp;
+use std::io::{Write, Read};
+use rmp::{encode,decode};
 use builder::Builder;
 use table::Table;
 use code_serialize::CodeSerialize;
+use code_deserialize::CodeDeserialize;
 
 pub struct Code<T: fmt::Debug> {
     pub symbols: Vec<(usize, String)>,
@@ -119,38 +120,104 @@ impl<T: CodeSerialize + fmt::Debug> CodeSerialize for Code<T> {
     /// ```
     fn to_byte_code(&self, mut buf: &mut Write) {
         // We're creating a 4-element map.
-        rmp::encode::write_map_len(&mut buf, 4).unwrap();
+        encode::write_map_len(&mut buf, 4).unwrap();
 
         // First, the code.
-        rmp::encode::write_str(&mut buf, "code").unwrap();
-        rmp::encode::write_array_len(&mut buf, self.code.len() as u32).unwrap();
+        encode::write_str(&mut buf, "code").unwrap();
+        encode::write_array_len(&mut buf, self.code.len() as u32).unwrap();
         for operation in self.code() {
-            rmp::encode::write_uint(&mut buf, *operation as u64).unwrap();
+            encode::write_uint(&mut buf, *operation as u64).unwrap();
         }
 
         // Next, the data.
-        rmp::encode::write_str(&mut buf, "data").unwrap();
-        rmp::encode::write_array_len(&mut buf, self.data.len() as u32).unwrap();
+        encode::write_str(&mut buf, "data").unwrap();
+        encode::write_array_len(&mut buf, self.data.len() as u32).unwrap();
         for operand in self.data() {
             operand.to_byte_code(&mut buf);
         }
 
         // Next, the symbols.
-        rmp::encode::write_str(&mut buf, "symbols").unwrap();
-        rmp::encode::write_array_len(&mut buf, (self.symbols.len() * 2) as u32).unwrap();
+        encode::write_str(&mut buf, "symbols").unwrap();
+        encode::write_array_len(&mut buf, (self.symbols.len() * 2) as u32).unwrap();
         for symbol in self.symbols() {
-            rmp::encode::write_uint(&mut buf, symbol.0 as u64).unwrap();
-            rmp::encode::write_str(&mut buf, &symbol.1).unwrap();
+            encode::write_uint(&mut buf, symbol.0 as u64).unwrap();
+            encode::write_str(&mut buf, &symbol.1).unwrap();
         }
 
         // Lastly, the labels.
-        rmp::encode::write_str(&mut buf, "labels").unwrap();
-        rmp::encode::write_array_len(&mut buf, (self.labels.len() * 2) as u32).unwrap();
+        encode::write_str(&mut buf, "labels").unwrap();
+        encode::write_array_len(&mut buf, (self.labels.len() * 2) as u32).unwrap();
         for label in self.labels() {
-            rmp::encode::write_uint(&mut buf, label.0 as u64).unwrap();
-            rmp::encode::write_str(&mut buf, &label.1).unwrap();
+            encode::write_uint(&mut buf, label.0 as u64).unwrap();
+            encode::write_str(&mut buf, &label.1).unwrap();
         }
     }
+}
+
+impl<T: CodeDeserialize + fmt::Debug> CodeDeserialize for Code<T> {
+    fn from_byte_code(mut buf: &mut Read) -> Code<T> {
+        // We expect a four-element map.
+        let map_len = decode::read_map_len(&mut buf).unwrap();
+        assert_eq!(map_len, 4);
+
+        // We expect the code section next:
+        let section = read_string(&mut buf);
+        assert_eq!(section, "code");
+
+        let code_len = decode::read_array_len(&mut buf).unwrap();
+        let mut code: Vec<usize> = vec![];
+        for _i in 0..code_len {
+            code.push(decode::read_int(&mut buf).unwrap());
+        }
+
+        // We expect the data section next
+        let section = read_string(&mut buf);
+        assert_eq!(section, "data");
+
+        let data_len = decode::read_array_len(&mut buf).unwrap();
+        let mut data: Vec<T> = vec![];
+        for _i in 0..data_len {
+            data.push(CodeDeserialize::from_byte_code(&mut buf));
+        }
+
+        // Next, symbols.
+        let section = read_string(&mut buf);
+        assert_eq!(section, "symbols");
+
+        let symbol_len = decode::read_array_len(&mut buf).unwrap();
+        let mut symbols: Vec<(usize, String)> = vec![];
+        for _i in 0..symbol_len / 2 {
+            let idx = decode::read_int(&mut buf).unwrap();
+            let symbol = read_string(&mut buf);
+            symbols.push((idx, symbol));
+        }
+
+        // Lastly, labels.
+        let section = read_string(&mut buf);
+        assert_eq!(section, "labels");
+
+        let label_len = decode::read_array_len(&mut buf).unwrap();
+        let mut labels: Vec<(usize, String)> = vec![];
+        for _i in 0..label_len / 2 {
+            let idx = decode::read_int(&mut buf).unwrap();
+            let label = read_string(&mut buf);
+            labels.push((idx, label));
+        }
+
+        Code {
+            symbols: symbols,
+            code:    code,
+            data:    data,
+            labels:  labels
+        }
+    }
+}
+
+fn read_string(mut buf: &mut Read) -> String {
+    let len = decode::read_str_len(&mut buf).unwrap();
+    let mut strbuf: Vec<u8> = vec![0u8; len as usize];
+    buf.read_exact(&mut strbuf).unwrap();
+    String::from_utf8(strbuf).unwrap()
 }
 
 #[cfg(test)]
@@ -163,7 +230,13 @@ mod test {
 
     impl CodeSerialize for usize {
         fn to_byte_code(&self, mut buf: &mut Write) {
-            rmp::encode::write_uint(&mut buf, *self as u64).unwrap();
+            encode::write_uint(&mut buf, *self as u64).unwrap();
+        }
+    }
+
+    impl CodeDeserialize for usize {
+        fn from_byte_code(mut buf: &mut Read) -> usize {
+            decode::read_int(&mut buf).unwrap()
         }
     }
 
@@ -250,5 +323,21 @@ mod test {
                         164, 109, 97, 105, 110, 8, 173, 115, 111, 109, 101, 95,
                         102, 117, 110, 99, 116, 105, 111, 110];
         assert_eq!(&actual[..], &expected[..]);
+    }
+
+    #[test]
+    fn from_byte_code() {
+        let bytecode: [u8; 82] = [132, 164, 99, 111, 100, 101, 154, 0, 0, 1, 1, 0, 1, 1,
+                    1, 2, 0, 164, 100, 97, 116, 97, 146, 123, 205, 1, 200,
+                    167, 115, 121, 109, 98, 111, 108, 115, 150, 0, 164,
+                    110, 111, 111, 112, 1, 164, 112, 117, 115, 104, 2, 163,
+                    112, 111, 112, 166, 108, 97, 98, 101, 108, 115, 148, 0,
+                    164, 109, 97, 105, 110, 8, 173, 115, 111, 109, 101, 95,
+                    102, 117, 110, 99, 116, 105, 111, 110];
+        let code: Code<usize> = Code::from_byte_code(&mut &bytecode[..]);
+        assert_eq!(code.code, [0x0, 0x0, 0x1, 0x1, 0x0, 0x1, 0x1, 0x1, 0x2, 0x0]);
+        assert_eq!(code.data, [123, 456]);
+        assert_eq!(code.symbols, [(0 as usize, "noop".to_string()), (1 as usize, "push".to_string()), (2 as usize, "pop".to_string())]);
+        assert_eq!(code.labels, [(0 as usize, "main".to_string()), (8 as usize, "some_function".to_string())])
     }
 }
