1c12b3b642ddf61f6213dced51e406c583b658f5
James Harton <james@automat.nz>
2017-11-13 21:39:17 +1300

    Implement serialization of code into bytecode.

--- src/code.rs
+++ src/code.rs
@@ -1,6 +1,9 @@
 use std::fmt;
+use std::io::Write;
+use rmp;
 use builder::Builder;
 use table::Table;
+use code_serialize::CodeSerialize;
 
 pub struct Code<T: fmt::Debug> {
     pub symbols: Vec<(usize, String)>,
@@ -99,12 +102,67 @@ impl<T: fmt::Debug> fmt::Debug for Code<T> {
     }
 }
 
+impl<T: CodeSerialize + fmt::Debug> CodeSerialize for Code<T> {
+    /// Create bytecode for this `Code`.
+    ///
+    /// Encodes into a Map of the following format:
+    /// ```json
+    /// {
+    ///     "code" => [ 0, 1, 0, 0, 1, 1, 1, 0 ],
+    ///     "data" => [ 123, 456 ],
+    ///     "symbols" => [ 0, "push", 1, "add" ],
+    ///     "labels" => [ 0, "main" ]
+    /// }
+    /// ```
+    fn to_byte_code(&self, mut buf: &mut Write) {
+        // We're creating a 4-element map.
+        rmp::encode::write_map_len(&mut buf, 4).unwrap();
+
+        // First, the code.
+        rmp::encode::write_str(&mut buf, "code").unwrap();
+        rmp::encode::write_array_len(&mut buf, self.code.len() as u32).unwrap();
+        for operation in self.code() {
+            rmp::encode::write_uint(&mut buf, *operation as u64).unwrap();
+        }
+
+        // Next, the data.
+        rmp::encode::write_str(&mut buf, "data").unwrap();
+        rmp::encode::write_array_len(&mut buf, self.data.len() as u32).unwrap();
+        for operand in self.data() {
+            operand.to_byte_code(&mut buf);
+        }
+
+        // Next, the symbols.
+        rmp::encode::write_str(&mut buf, "symbols").unwrap();
+        rmp::encode::write_array_len(&mut buf, (self.symbols.len() * 2) as u32).unwrap();
+        for symbol in self.symbols() {
+            rmp::encode::write_uint(&mut buf, symbol.0 as u64).unwrap();
+            rmp::encode::write_str(&mut buf, &symbol.1).unwrap();
+        }
+
+        // Lastly, the labels.
+        rmp::encode::write_str(&mut buf, "labels").unwrap();
+        rmp::encode::write_array_len(&mut buf, (self.labels.len() * 2) as u32).unwrap();
+        for label in self.labels() {
+            rmp::encode::write_uint(&mut buf, label.0 as u64).unwrap();
+            rmp::encode::write_str(&mut buf, &label.1).unwrap();
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     use instruction::Instruction;
     use instruction_table::InstructionTable;
     use machine::Machine;
+    use code_serialize::CodeSerialize;
+
+    impl CodeSerialize for usize {
+        fn to_byte_code(&self, mut buf: &mut Write) {
+            rmp::encode::write_uint(&mut buf, *self as u64).unwrap();
+        }
+    }
 
     fn noop(_machine: &mut Machine<usize>, _args: &[usize]) {}
 
@@ -167,5 +225,27 @@ mod test {
 \tpop
 ";
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn to_byte_code() {
+        let it = example_instruction_table();
+        let mut builder: Builder<usize> = Builder::new(&it);
+        builder.push(0, vec![]);
+        builder.push(1, vec![123]);
+        builder.push(1, vec![456]);
+        builder.label("some_function");
+        builder.push(2, vec![]);
+        let code = Code::from_builder(builder);
+        let mut actual: Vec<u8> = vec![];
+        code.to_byte_code(&mut actual);
+        let expected = [132, 164, 99, 111, 100, 101, 154, 0, 0, 1, 1, 0, 1, 1,
+                        1, 2, 0, 164, 100, 97, 116, 97, 146, 123, 205, 1, 200,
+                        167, 115, 121, 109, 98, 111, 108, 115, 150, 0, 164,
+                        110, 111, 111, 112, 1, 164, 112, 117, 115, 104, 2, 163,
+                        112, 111, 112, 166, 108, 97, 98, 101, 108, 115, 148, 8,
+                        173, 115, 111, 109, 101, 95, 102, 117, 110, 99, 116,
+                        105, 111, 110, 0, 164, 109, 97, 105, 110];
+        assert_eq!(&actual[..], &expected[..]);
     }
 }
